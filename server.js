1a6888f6d1ec46df4aaab7f0027abe9c374a9303
tocanto <timmyocanto@gmail.com>
2021-07-04 21:03:16 -0400

    How to: Git and Github

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root : __dirname});
+});
+
+app.use(express.static(__dirname + '/dist'));
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+        console.log(__dirname);
+        console.log("Listing Port " + port);
+});
