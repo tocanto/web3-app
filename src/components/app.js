9acc0eb47795f35933628dc4d4b05b4bc5c773ab
tocanto <timmyocanto@gmail.com>
2021-07-20 22:17:06 -0400

    Agregando Waves Keeper a la aplicación

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Signer } from '@waves/signer';
+import { ProviderWeb } from '@waves.exchange/provider-web';
+
+const signer = new Signer();
+signer.setProvider(new ProviderWeb());
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
         this.authFunc = this.authFunc.bind(this);
+        this.authSigner = this.authSigner.bind(this);
     }
 
     authFunc() {
@@ -23,10 +29,24 @@ class App extends React.Component {
         }
     }
 
+    authSigner() {    
+            const userData = signer.login()  // calling Waves Signer
+            .then(user => {
+                console.log(user); //Displaying te result on the console
+                /*...Proccesing data */
+                this.setState(user);
+            }).catch(error => { //Displaying the result on the console
+                /*Proccesing erros */
+                console.error('login rejected')
+            })
+    }
+
     render() {
         return (
             <div className="container">
                 <input className="btn btn-primary" type="submit" value="Auth" onClick={this.authFunc}/>
+                <input className="js-login btn btn-secondary" type="submit" value="AuthSigner" onClick={this.authSigner}/>
+                <p>Waves Address who gave authorization with Waves Keeper: <strong>{this.state.address}</strong></p>
             </div>
         )
     }
